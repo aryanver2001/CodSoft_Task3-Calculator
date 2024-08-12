856321318d61cfe2269d05db2724e0c7f843ce11
ARYAN VERMA <aryanver6@gmail.com>
2024-08-13 01:11:50 +0530

    MODIFIED

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -12,38 +12,38 @@ export default function Calculator() {
                 <div className="btn-grp">
                     
                     <div className='btn-r1'>
-                        <button type="button" className="btn btn-outline-success btns">Clear</button>
-                        <button type="button" className="btn btn-outline-success btns">Check</button>
-                        <button type="button" className="btn btn-outline-success btns">%</button>
-                        <button type="button" className="btn btn-outline-success btns">=</button>
+                        <button type="button" className="btn btn-outline-info btns">Clear</button>
+                        <button type="button" className="btn btn-outline-info btns">Delete</button>
+                        <button type="button" className="btn btn-outline-info btns">%</button>
+                        <button type="button" className="btn btn-outline-info btns">=</button>
                     </div>
 
                     <div className="btns-r2">
-                        <button type="button" className="btn btn-outline-success btns">1</button>
-                        <button type="button" className="btn btn-outline-success btns">2</button>
-                        <button type="button" className="btn btn-outline-success btns">3</button>
-                        <button type="button" className="btn btn-outline-success btns">/</button>
+                        <button type="button" className="btn btn-outline-info btns">1</button>
+                        <button type="button" className="btn btn-outline-info btns">2</button>
+                        <button type="button" className="btn btn-outline-info btns">3</button>
+                        <button type="button" className="btn btn-outline-info btns">/</button>
                     </div>
 
                     <div className="btns-r3">
-                        <button type="button" className="btn btn-outline-success btns">4</button>
-                        <button type="button" className="btn btn-outline-success btns">5</button>
-                        <button type="button" className="btn btn-outline-success btns">6</button>
-                        <button type="button" className="btn btn-outline-success btns">X</button>
+                        <button type="button" className="btn btn-outline-info btns">4</button>
+                        <button type="button" className="btn btn-outline-info btns">5</button>
+                        <button type="button" className="btn btn-outline-info btns">6</button>
+                        <button type="button" className="btn btn-outline-info btns">X</button>
                     </div>
 
                     <div className="btns-r4">
-                        <button type="button" className="btn btn-outline-success btns">7</button>
-                        <button type="button" className="btn btn-outline-success btns">8</button>
-                        <button type="button" className="btn btn-outline-success btns">9</button>
-                        <button type="button" className="btn btn-outline-success btns">-</button>
+                        <button type="button" className="btn btn-outline-info btns">7</button>
+                        <button type="button" className="btn btn-outline-info btns">8</button>
+                        <button type="button" className="btn btn-outline-info btns">9</button>
+                        <button type="button" className="btn btn-outline-info btns">-</button>
                     </div>
 
                     <div className="btns-r5">
-                        <button type="button" className="btn btn-outline-success btns">0</button>
-                        <button type="button" className="btn btn-outline-success btns">00</button>
-                        <button type="button" className="btn btn-outline-success btns">.</button>
-                        <button type="button" className="btn btn-outline-success btns">+</button>
+                        <button type="button" className="btn btn-outline-info btns">0</button>
+                        <button type="button" className="btn btn-outline-info btns">00</button>
+                        <button type="button" className="btn btn-outline-info btns">.</button>
+                        <button type="button" className="btn btn-outline-info btns">+</button>
                     </div>
 
                 </div>
